59d1e4dee2d0fe82f57e24a9abc7a7f6b7060d47
Devin Shende <devin.s.shende@gmail.com>
2025-10-22 10:54:17 -0700

    config file

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://devinshende.github.io',
+  // No 'base' property is needed for a username.github.io repo
+});
